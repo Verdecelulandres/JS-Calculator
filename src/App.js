fda519926e267faf8b546787db37161f4aa78d05
AndresLa <andres.laverde@protonmail.com>
2024-05-13 19:12:10 -0700

    Add all buttons

--- src/App.js
+++ src/App.js
@@ -18,9 +18,32 @@ class App extends React.Component {
         <div className='calculator-body'>
           <div className='display'>
             <div className='full-operation'></div>
-            <div className='current-value'></div>
+            <div className='current-value'>{this.state.currentDisplay}</div>
           </div>
-          <div className='btn-container'></div>
+          <div className='btn-container'>
+            {/* First row */}
+            <button id="clear"></button>
+            <button id="multiply"></button>
+            {/* second row */}
+            <button id="seven"></button>
+            <button id="eight"></button>
+            <button id="nine"></button>
+            <button id="divide"></button>
+            {/* Third row */}
+            <button id="four"></button>
+            <button id="five"></button>
+            <button id="six"></button>
+            <button id="subtract"></button>
+            {/* Fourth row */}
+            <button id="one"></button>
+            <button id="two"></button>
+            <button id="three"></button>
+            <button id="add"></button>
+            {/* Fifth row */}
+            <button id="zero"></button>
+            <button id="decimal"></button>
+            <button id="equals"></button>
+          </div>
         </div>
       </div>
     );
